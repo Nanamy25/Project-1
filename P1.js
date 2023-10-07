4c2f78f7e035889452d121f23128f786b4ee79e2
Annabell <annabellrambla@gmail.com>
2023-10-07 14:40:33 -0400

    Fixed everything: code, and indentation

--- P1.js
+++ P1.js
@@ -7,27 +7,43 @@ userChoiceRounds = prompt("How many rounds do you want to play?");
 var rounds = parseInt(userChoiceRounds);
 var currentRound = 0;
 
+let wins = 0;
+let tied = 0;
+let lost = 0;
+
 while (currentRound < rounds){
+    while (true) {
 userChoice = prompt("Do you pick rock, paper, scissors? (enter r, p, or s)");
+if (userChoice=== "r" || userChoice=== "p" ||userChoice=== "s") {
+    break;
+} else {
+    alert("Wrong choice there buddy! Input r, p, or s!!")
+}
+}
 
-
-// for(let i = 0; i < rounds; i++){ 
+ 
 var randomNumber = Math.floor(Math.random()*3);
  if (randomNumber == 0) {computerChoice = "r"}
 else if (randomNumber == 1) {computerChoice = "p"}
 else {computerChoice = "s"}
 
 if (userChoice == computerChoice) 
-{alert ("You tied.")
+{ 
+    tied++;
+    alert ("You tied.")
 } else if (
     (userChoice === "r" && computerChoice === "p") ||
     (userChoice === "s" && computerChoice === "r") ||
     (userChoice === "p" && computerChoice === "s") 
-) {
+) { 
+    lost++;
     alert("You Lost. You picked " +userChoice +" which does not beat " +computerChoice)
-} else {
+} else { 
+    wins++;
     alert("You Won. You picked " +userChoice +" which beats " +computerChoice)
 }
 currentRound++;
 }
 
+
+alert("You played " +userChoiceRounds +" rounds. In those rounds you won " +wins +" times, you tied " +tied +" times, you lost " +lost +" times")
